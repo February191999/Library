389e3d243f1c0ca17bc5a3a31a2a86f491b212dd
February191999 <96108108+February191999@users.noreply.github.com>
2025-10-14 00:16:10 +0700

    Add comments

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
-const myLibrary = [
-    {
-        title: "harry",
-        author: "Jk",
-        pages: 29,
-        read: "read"
-    }
-];
-const bookTitle = document.querySelector("#book-title");
-const bookAuthor = document.querySelector("#book-author");
-const bookPages = document.querySelector("#book-pages");
-const bookStatus = document.querySelector("#book-status");
-const submitButton = document.querySelector(".submit-btn");
-const cardOutput = document.querySelector(".card-display");
-
-const card = document.createElement("div");
-
-card.setAttribute("style", "background-color: #FFFFFF; color: black; text-align: center; height: 150px; margin: 10px; border: 3px solid black")
+const myLibrary = [{}]; // Array for storing books
+const bookTitle = document.querySelector("#book-title"); // Book title
+const bookAuthor = document.querySelector("#book-author"); // Book author
+const bookPages = document.querySelector("#book-pages");   // Book pages
+const bookStatus = document.querySelector("#book-status");  // Book status
+const submitButton = document.querySelector(".submit-btn"); // Submit button
+const cardOutput = document.querySelector(".card-display"); 
 
 function Book(title, author, pages, read) {
     if (!new.target) {
@@ -27,7 +16,7 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-function addBookToLibrary(title, author, pages, read) {
+function addBookToLibrary(title, author, pages, read) { //Function to add books to library
 
     this.title = title;
     this.author = author;
@@ -36,23 +25,18 @@ function addBookToLibrary(title, author, pages, read) {
 
     const enteredBook = new Book(title, author, pages, read);
     
-    enteredBook.id = crypto.randomUUID();
-    myLibrary.push(enteredBook);
-
+    enteredBook.id = crypto.randomUUID(); // Gives each book a unique id
+    myLibrary.push(enteredBook); // Push book to library
 }
-
-addBookToLibrary("harry", "jk", 29, "read")
-addBookToLibrary("joker", "jk", 29, "read")
 
 function createCard() {
     
-    console.log("this print")
-    const newBook = myLibrary.at(-1);
-    
-    const card = document.createElement("div");
+    const newBook = myLibrary.at(-1); // indexes last book in library to get the latest book added
+
+    const card = document.createElement("div"); // Create card div
     card.setAttribute("style", "background-color: #FFFFFF; color: black; text-align: center; height: 150px; margin: 10px; border: 3px solid black")
     
-    const title = document.createElement("h2");
+    const title = document.createElement("h2"); 
     title.textContent = `${newBook.title}`;
     card.appendChild(title);
     const author = document.createElement("p");
@@ -63,50 +47,11 @@ function createCard() {
     card.appendChild(pages);
     const read = document.createElement("p");
     read.textContent = `Read: ${newBook.read}`;
-    card.appendChild(read);
-    cardOutput.appendChild(card);
+    card.appendChild(read); 
+    cardOutput.appendChild(card); // Display card in output div
 }
 
-function createCards() {
-
-    if (myLibrary.length <= 2) {
-
-        console.log("this print")
-
-        createCard();
-    } else {
-        myLibrary.forEach((book) => {
-        
-        console.log("print this")
-
-        const card = document.createElement("div");
-
-        card.setAttribute("style", "background-color: #FFFFFF; color: black; text-align: center; height: 150px; margin: 10px; border: 3px solid black")
-
-            
-        const title = document.createElement("h2");
-        title.textContent = `${book.title}`;
-        card.appendChild(title);
-
-        const author = document.createElement("p");
-        author.textContent = `Author: ${book.author}`;
-        card.appendChild(author);
-
-        const pages = document.createElement("p");
-        pages.textContent = `Pages: ${book.pages}`;
-        card.appendChild(pages);
-
-        const read = document.createElement("p");
-        read.textContent = `Read: ${book.read}`;
-        card.appendChild(read);
-
-        cardOutput.appendChild(card);
-    }
-)}}
-
-createCards();
-
-submitButton.addEventListener("click", (event) => {
+submitButton.addEventListener("click", (event) => { // Click event for submit button
     console.log(addBookToLibrary(bookTitle.value, bookAuthor.value, bookPages.value, bookStatus.value));
     console.log(myLibrary);
 
